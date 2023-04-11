140d4e16574cb2b1e99ad0ff3717854e618f3219
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-04-11 16:03:24 +0200

    unfavorite function on Listed Restaurants

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -8,7 +8,7 @@ import {
 } from "../../utils/google_places";
 
 const Title = styled.h1`
-  margin-top: 100px; /* ajusta el margen superior según sea necesario */
+  margin-top: 100px;
 `;
 
 const StyledListContainer = styled.div`
@@ -27,6 +27,18 @@ export default function LatinRestaurants({ restaurants }) {
     setFavorites(favorites);
   }, []);
 
+  const handleUnfavorite = (place_id) => {
+    const newFavorites = favorites.filter((id) => id !== place_id);
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    setFavorites(newFavorites);
+  };
+
+  const handleFavorite = (place_id) => {
+    const newFavorites = [...favorites, place_id];
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    setFavorites(newFavorites);
+  };
+
   return (
     <Layout title="Latin Restaurants">
       <StyledListContainer>
@@ -35,6 +47,8 @@ export default function LatinRestaurants({ restaurants }) {
             key={restaurant.place_id}
             restaurant={restaurant}
             isFavorite={favorites.includes(restaurant.place_id)}
+            onFavorite={handleFavorite}
+            onUnfavorite={handleUnfavorite}
           />
         ))}
       </StyledListContainer>
