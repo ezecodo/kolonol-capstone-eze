c6cc818122973cece586b6eeb2b248806a9a77a9
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-04-14 01:55:34 +0200

    add place note

--- components/PlaceCard/index.js
+++ components/PlaceCard/index.js
@@ -14,6 +14,69 @@ const Card = styled.div`
   overflow: hidden;
 `;
 
+const AddNoteButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  background-color: ${({ isOpen }) => (isOpen ? "red" : "#15aabf")};
+  border: none;
+  cursor: pointer;
+  color: white;
+  font-size: 18px;
+  padding: 5px 10px;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NoteWindow = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #15aabf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const NoteFormName = styled.h3`
+  margin: 0 0 10px 0;
+  text-align: center;
+  width: 100%;
+`;
+const NoteForm = styled.form`
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1);
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const NoteInput = styled.textarea`
+  width: 100%;
+  height: 60px;
+  resize: none;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+const SubmitNoteButton = styled.button`
+  margin-top: 10px;
+  background-color: #15aabf;
+  color: white;
+  border: none;
+  cursor: pointer;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
+
 const Details = styled.div`
   padding: 20px;
 `;
@@ -77,11 +140,27 @@ const BookmarkIconFull = styled.span`
 function PlaceCard({ place, isFavorite, onToggleFavorite }) {
   const [bookmarked, setBookmarked] = useState(false);
   const [ratingStars, setRatingStars] = useState(0);
+  const [showNoteWindow, setShowNoteWindow] = useState(false);
+  const [note, setNote] = useState("");
 
   useEffect(() => {
     setBookmarked(isFavorite);
     setRatingStars(Math.round(place.rating));
   }, [isFavorite, place.rating]);
+
+  const handleAddNoteClick = () => {
+    setShowNoteWindow(!showNoteWindow);
+  };
+
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  };
+  const handleSubmitNote = (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar la nota, por ejemplo, a una API o guardarla en el estado de un componente padre
+    console.log("Nota enviada:", note);
+    setShowNoteWindow(false);
+  };
 
   const name = place.name;
   const description = place.description || place.types.join(", ");
@@ -114,6 +193,22 @@ function PlaceCard({ place, isFavorite, onToggleFavorite }) {
           ))}
         </RatingContainer>
       </Details>
+      {showNoteWindow && (
+        <NoteWindow>
+          <NoteForm onSubmit={handleSubmitNote}>
+            <NoteFormName>{name}</NoteFormName>
+            <NoteInput
+              placeholder="Escribe una nota sobre el lugar..."
+              value={note}
+              onChange={handleNoteChange}
+            />
+            <SubmitNoteButton type="submit">Enviar nota</SubmitNoteButton>
+          </NoteForm>
+        </NoteWindow>
+      )}
+      <AddNoteButton onClick={handleAddNoteClick} isOpen={showNoteWindow}>
+        {showNoteWindow ? "×" : "+"}
+      </AddNoteButton>
     </Card>
   );
 }
