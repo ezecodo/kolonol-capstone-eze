990413ea15baf8b583993a5a37a3881ade9d2cf7
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-04-14 12:49:21 +0200

    add categories style

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,37 +1,86 @@
 import React from "react";
-import Link from "next/link";
 import styled from "styled-components";
+import Link from "next/link";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  background-color: #15aabf;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
+  font-size: 15px;
+  color: white;
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: inherit;
+  &:hover {
+    text-decoration: none;
+  }
+`;
 
 const Container = styled.div`
-  margin-top: 120px;
   display: flex;
-  justify-content: space-between;
+  margin-top: 45px;
   align-items: center;
-  background-color: #15aabf;
-  color: #fff;
-  font-size: 0.7rem;
-  max-width: 100%;
-  height: 50px;
-  padding: 1rem;
-  box-shadow: 0px -5px 10px 0px rgba(0, 0, 0, 0.1);
+  justify-content: center;
+  height: calc(100vh - 80px);
+  overflow: auto;
 `;
 
-const StyledLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  margin-left: 1rem;
-  margin-right: 1rem;
-  cursor: pointer;
-`;
-
-const FavCategories = () => (
-  <Container>
-    <StyledLink href="/restaurants/FavoriteRestaurants">
-      Restaurantes
-    </StyledLink>
-    <StyledLink href="/night-life/FavoriteClubs">Música</StyledLink>
-    <StyledLink href="#">Teatro</StyledLink>
-  </Container>
-);
+function FavCategories() {
+  return (
+    <Container>
+      <Wrapper>
+        <Link href="/restaurants/FavoriteRestaurants">
+          <StyledLink>
+            <Button>Latin Restaurants</Button>
+          </StyledLink>
+        </Link>
+        <Link href="/night-life/FavoriteClubs">
+          <StyledLink>
+            <Button>Latin Music</Button>
+          </StyledLink>
+        </Link>
+        <Link href="/latin-music">
+          <StyledLink>
+            <Button>Tandem</Button>
+          </StyledLink>
+        </Link>
+        <Link href="/">
+          <StyledLink>
+            <Button>Random Text 1</Button>
+          </StyledLink>
+        </Link>
+        <Link href="/">
+          <StyledLink>
+            <Button>Random Text 2</Button>
+          </StyledLink>
+        </Link>
+        <Link href="/">
+          <StyledLink>
+            <Button>Random Text 3</Button>
+          </StyledLink>
+        </Link>
+      </Wrapper>
+    </Container>
+  );
+}
 
 export default FavCategories;
